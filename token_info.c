7c65ceb72c15d15f7fe8a86e8414e1d8f6003b15
Gianluca Mazzini <gianluca@mazzini.org>
2025-11-19 19:55:50 +0100

    Update token_info.c

--- token_info.c
+++ token_info.c
@@ -2,78 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 #include <curl/curl.h>
+#BUFOUT 5000
 
-
-struct memory {
-    char *response;
-    size_t size;
-};
-
-/* libcurl richiede per forza una callback */
-static size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp) {
-    size_t realsize = size * nmemb;
-    struct memory *mem = (struct memory *)userp;
-
-    char *ptr = realloc(mem->response, mem->size + realsize + 1);
-    if(ptr == NULL) {
-        return 0;  // errore allocazione
-    }
-
-    mem->response = ptr;
-    memcpy(&(mem->response[mem->size]), contents, realsize);
-    mem->size += realsize;
-    mem->response[mem->size] = 0;
-
-    return realsize;
+int newout=1;
+static size_t write_cb2(void *ptr,size_t size,size_t nmemb,void *userdata){
+  size_t realsize=size*nmemb;
+  char **buffer=(char **)userdata;
+  static char *out=NULL;
+  static size_t actpos=0;
+  if(out==NULL){
+    out=(char *)malloc(BUFOUT*sizeof(char));
+    if(out==NULL)return 0;
+  }
+  if(newout){actpos=0; newout=0;}
+  memcpy(out+actpos,ptr,realsize);
+  actpos+=realsize;
+  *(out+actpos)='\0'; 
+  *buffer=out;
+  return realsize;
 }
 
-
 int main(){
-  char access_token[512],url[512];
+  char access_token[512],url[512],*out;
   FILE *fp;
   CURL *curl;
   CURLcode res;
-  struct memory chunk;
-
-
+  
   fp=fopen("/home/www/data/access_token","r");
   if(!fp)return 0;
   if(!fgets(access_token,512,fp)){fclose(fp); return 0;}
   fclose(fp);
   access_token[strcspn(access_token,"\n")]='\0';
   sprintf(url,"https://oauth2.googleapis.com/tokeninfo?access_token=%s",access_token);
-
-    /* Inizializza libcurl */
-    curl_global_init(CURL_GLOBAL_DEFAULT);
-    curl = curl_easy_init();
-
-    if (!curl) {
-        fprintf(stderr, "Errore: impossibile inizializzare curl\n");
-        return 1;
-    }
-
-    chunk.response = malloc(1);
-    chunk.size = 0;
-
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
-
-    /* Evita verifica SSL come nel tuo PHP */
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-
-    /* Esegui richiesta */
-    res = curl_easy_perform(curl);
-
-    if(res != CURLE_OK) {
-        fprintf(stderr, "Errore curl: %s\n", curl_easy_strerror(res));
-    } else {
-        printf("%s\n", chunk.response);
-    }
-
-    free(chunk.response);
-    curl_easy_cleanup(curl);
-    curl_global_cleanup();
-
-    return 0;
+  curl_global_init(CURL_GLOBAL_DEFAULT);
+  curl=curl_easy_init();
+  if(!curl)return 0;
+  curl_easy_setopt(curl,CURLOPT_URL,url);
+  curl_easy_setopt(curl,CURLOPT_WRITEFUNCTION,write_cb2);
+  curl_easy_setopt(curl,CURLOPT_WRITEDATA,&out);
+  curl_easy_setopt(curl,CURLOPT_SSL_VERIFYPEER,0L);
+  res=curl_easy_perform(curl);
+  if(res!=CURLE_OK)return 0;
+  printf("%s\n",out);
+  curl_easy_cleanup(curl);
+  curl_global_cleanup();
+  return 1;
 }
