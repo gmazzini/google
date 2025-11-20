cc1f17518232ba14cc2e576479553d826198f6dd
Gianluca Mazzini <gianluca@mazzini.org>
2025-11-20 06:09:36 +0100

    Create token_start.c

---
+++ token_start.c
@@ -0,0 +1,9 @@
+#include "func.c"
+
+int main(void) {
+  char url[512];
+  sprintf(url,"https://accounts.google.com/o/oauth2/auth?scope=%s&access_type=offline&response_type=code&redirect_uri=%s&client_id=%s",encoded(scope),redirect_uri,client_id);
+  printf("Status: 302 Found\r\n");
+  printf("Location: %s\r\n", googleOauthURL);
+  printf("\r\n");
+}
