9da14e3dce705abb4af7388172c7756523a5a179
Gianluca Mazzini <gianluca@mazzini.org>
2025-11-20 13:11:35 +0100

    Update token_auth.c

--- token_auth.c
+++ token_auth.c
@@ -1,9 +1,10 @@
 #include "func.c"
 
 int main(){
-  char *in,*p1,*p2,url[512],post[512],*out;
+  char *in,*p1,*p2,*p3,url[512],post[512],tok[30],*out;
   CURL *curl;
   CURLcode res;
+  FILE *fp;
 
   printf("Content-Type: text/plain\r\n\r\n");
   if(strcmp(getenv("REQUEST_METHOD"),"GET")!=0)return 0;
@@ -14,9 +15,6 @@ int main(){
   p1=p2;
   while(*p1!='\0' && *p1!='&' && *p1!='\n')p1++;
   *p1='\0';
-  FILE *fp2; fp2=fopen("/home/www/google/q2.txt","w");
-  fprintf(fp2,"%s\n%s\n",in,p2);
-  fclose(fp2);
   sprintf(post,"client_id=%s&redirect_uri=%s&client_secret=%s&code=%s&access_type=offline&grant_type=authorization_code",client_id,redirect_uri,client_secret,p2);
   sprintf(url,"https://oauth2.googleapis.com/token");
   curl=curl_easy_init();
@@ -29,8 +27,27 @@ int main(){
   curl_easy_setopt(curl,CURLOPT_POSTFIELDS,post);
   res=curl_easy_perform(curl);
   if(res!=CURLE_OK)return 0;
-  FILE *fp; fp=fopen("/home/www/google/q1.txt","w");
-  fprintf(fp,"%s\n",out);
+  strcpy(tok,"\"access_token\": \"");
+  p1=strstr(out,tok);
+  if(p1==NULL)return 0;
+  access_token=p1+strlen(tok);
+  p2=strstr(access_token,"\"");
+  if(p2==NULL)return 0;
+  *p2='\0';
+  strcpy(tok,"\"refresh_token\": \"");
+  p1=strstr(out,tok);
+  if(p1==NULL)return 0;
+  refresh_token=p1+strlen(tok);
+  p2=strstr(access_token,"\"");
+  if(p2==NULL)return 0;
+  *p2='\0';
+  fp=fopen("/home/www/data/google_access_token","w");
+  if(fp==NULL){curl_easy_cleanup(curl); return 0;}
+  fprintf(fp,"%s\n",access_token);
+  fclose(fp);
+  fp=fopen("/home/www/data/google_refresh_token","w");
+  if(fp==NULL){curl_easy_cleanup(curl); return 0;}
+  fprintf(fp,"%s\n",refresh_token);
   fclose(fp);
   curl_easy_cleanup(curl);
   return 1;
