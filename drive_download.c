c5769e8a851cf82efcc6085fdba75b3b9e627e70
Gianluca Mazzini <gianluca@mazzini.org>
2025-11-21 07:01:45 +0100

    Update drive_download.c

--- drive_download.c
+++ drive_download.c
@@ -1,11 +1,12 @@
 #include "func.c"
 
 int main(int argc,char *argv[]){
-  char access_token[512],url[512],auth_header[512],query[512],*out;
+  char access_token[512],url[512],auth_header[512],query[512],*id,*out;
   FILE *fp;
   CURL *curl;
   CURLcode res;
   struct curl_slist *headers;
+  int i;
 
   fp=fopen("/home/www/data/google_access_token","r");
   if(!fp)return 0;
@@ -19,7 +20,6 @@ int main(int argc,char *argv[]){
   headers=curl_slist_append(headers,auth_header);
   sprintf(query,"name='%s' and '%s' in parents",curl_easy_escape(curl,argv[1],0),curl_easy_escape(curl,argv[2],0));
   sprintf(url,"https://www.googleapis.com/drive/v3/files?q=%s",myencode(query));
-  printf("%s\n",url);
   curl=curl_easy_init();
   if(!curl)return 0;
   curl_easy_setopt(curl,CURLOPT_URL,url);
@@ -30,6 +30,20 @@ int main(int argc,char *argv[]){
   res=curl_easy_perform(curl);
   if(res!=CURLE_OK)return 0;
   printf("%s\n",out);
+
+  for(i=0;;i++){
+    strcpy(tok,"\"id\": \"");
+    p1=strstr(out,tok);
+    if(p1==NULL)break;
+    id=p1+strlen(tok);
+    p2=strstr(id,"\"");
+    if(p2==NULL)break;
+  }
+  if(i!=1)return 0;
+  *p2='\0';
+
+printf("%s\n",id);
+  
   curl_slist_free_all(headers);
   curl_easy_cleanup(curl);
   return 1;
